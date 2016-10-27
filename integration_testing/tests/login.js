f741fee77c7c53911b0170f3a184cb0e2aaf3871
Alan <kiriost.s@gmail.com>
2016-10-27 04:28:25 -0300

    Algunos tests automatizados con CasperJS

--- integration_testing/tests/login.js
+++ integration_testing/tests/login.js
@@ -1,32 +1,38 @@
 // const casper = require('casper').create();
 
-casper.test.begin('Bloque de login debe existir', 4, function(test) {
+var loginFormSelector = 'form[action="login.php"][name="home"]';
+var userNameSelector = 'input[type="text"][name="userName"]';
+var passwordSelector = 'input[type="password"][name="password"]';
+var submitSelector = 'input[type="image"][name="login"]';
+
+/**
+ * El número indica la cantidad de tests que esperamos que pasen
+ */
+casper.test.begin('Login', 6, function(test) {
 
   casper.start('http://newtours.demoaut.com/', function() {
     test.assertTitle('Welcome: Mercury Tours', 'título de página correcto');
-    test.assertExists('form[name="home"]', 'formulario de login encontrado');
-    test.assertExists('input[name="userName"]', 'campo de usuario existe');
-    test.assertExists('input[name="password"]', 'campo contraseña existe');
+    test.assertExists(loginFormSelector, 'formulario de login encontrado');
+    test.assertExists(userNameSelector, 'campo de usuario existe');
+    test.assertExists(passwordSelector, 'campo contraseña existe');
+
+    this.sendKeys(userNameSelector, 'tutorial');
+    this.sendKeys(passwordSelector, 'tutorial');
+
+    test.assertEval(function(userNameSelector) {
+      return __utils__.getFieldValue(userNameSelector) === 'tutorial';
+    }, 'campo usuario con usuario "tutorial"', userNameSelector);
+
+    test.assertEval(function(passwordSelector) {
+      return __utils__.getFieldValue(passwordSelector) === 'tutorial';
+    }, 'campo contraseña con usuario "tutorial"', passwordSelector);
+
+    this.click(submitSelector);
   });
 
   casper.then(function() {
-    this.fill('form[action="login.php"]', {
-      userName: 'tutorial',
-      password: '123456'
-    }, true);
-
-    this.click('input[name="login"]');
-
+    test.assertExists('a[href="mercurysignoff.php"]', 'existe botón para deslogueo');
   });
-
-  // casper.then(function() {
-  //   test.assertTitle("casperjs - Recherche Google", "google title is ok");
-  //   test.assertUrlMatch(/q=casperjs/, "search term has been submitted");
-  //   test.assertEval(function() {
-  //       return __utils__.findAll("h3.r").length >= 10;
-  //     },
-  //     "google search for \"casperjs\" retrieves 10 or more results");
-  // });
 
   casper.run(function() {
     test.done();
