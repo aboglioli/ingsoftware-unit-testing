762e57db6fe5aa42972f575dc426824d3d22b5b4
Alan <kiriost.s@gmail.com>
2016-10-27 04:50:02 -0300

    Arregla espera de casper para reservaciones

--- integration_testing/tests/login.js
+++ integration_testing/tests/login.js
@@ -8,13 +8,14 @@ var submitSelector = 'input[type="image"][name="login"]';
 /**
  * El número indica la cantidad de tests que esperamos que pasen
  */
-casper.test.begin('Login', 6, function(test) {
+casper.test.begin('Login', 9, function(test) {
 
   casper.start('http://newtours.demoaut.com/', function() {
     test.assertTitle('Welcome: Mercury Tours', 'título de página correcto');
     test.assertExists(loginFormSelector, 'formulario de login encontrado');
     test.assertExists(userNameSelector, 'campo de usuario existe');
     test.assertExists(passwordSelector, 'campo contraseña existe');
+    test.assertExists(submitSelector, 'botón para enviar existe');
 
     this.sendKeys(userNameSelector, 'tutorial');
     this.sendKeys(passwordSelector, 'tutorial');
@@ -27,10 +28,25 @@ casper.test.begin('Login', 6, function(test) {
       return __utils__.getFieldValue(passwordSelector) === 'tutorial';
     }, 'campo contraseña con usuario "tutorial"', passwordSelector);
 
-    this.click(submitSelector);
+    // this.fill(loginFormSelector, {
+    //   userName: 'tutorial',
+    //   password: 'tutorial'
+    // }, true)
+
+    this.click(submitSelector, '50%', '50%');
   });
 
   casper.then(function() {
+   // Esto queda vacío, es sólo para que casper espere a que cargue la nueva
+   // página a la que es redirigido (reservaciones)
+  });
+
+  casper.then(function() {
+    console.log('Página ' + this.getCurrentUrl());
+  });
+
+  casper.then(function() {
+    test.assertUrlMatch(/mercuryreservation\.php/, 'entró a reservaciones')
     test.assertExists('a[href="mercurysignoff.php"]', 'existe botón para deslogueo');
   });
 
